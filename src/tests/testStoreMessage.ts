c98d2190f88161a580d3162a31a1236abf5a176b
Adam Quigley <adam.quigley@versent.com.au>
2019-05-20 14:59:36 +1000

    move local dynamodb test logic into testUtils lib

--- src/tests/testStoreMessage.ts
+++ src/tests/testStoreMessage.ts
@@ -2,68 +2,21 @@ import {
     StoreMessageEventInput,
     handler
 } from '../storeMessage'
-import AWS from "aws-sdk"
 
-const ddbClient = new AWS.DynamoDB({
-  endpoint: "http://localhost:8000",
-    region: "ap-southeast-2"
-})
+import {
+    initDb,
+    teardownDb
+} from './testUtils/localDynamodb'
 
 
 describe("handler", () => {
 
     beforeAll(async () => {
-
         jest.setTimeout(60000);
-
-        const tableName = process.env.DYNAMODB_TABLE || "";
-
-        try {
-          await ddbClient.deleteTable({TableName: tableName}).promise()
-        } catch (err) {
-          if (err && err.code !== "ResourceNotFoundException") {
-            throw err
-          }
-        }
-
-        await ddbClient.waitFor("tableNotExists", {
-          TableName: tableName,
-        }).promise()
-
-        const createParams: AWS.DynamoDB.Types.CreateTableInput = {
-          TableName: tableName,
-          AttributeDefinitions: [
-            {AttributeName: "deviceId", AttributeType: "S"},
-            {AttributeName: "timestamp", AttributeType: "N"},
-          ],
-          KeySchema: [
-            {AttributeName: "deviceId", KeyType: "HASH"},
-            {AttributeName: "timestamp", KeyType: "RANGE"},
-          ],
-          ProvisionedThroughput: {
-            ReadCapacityUnits: 2,
-            WriteCapacityUnits: 2,
-          },
-        }
-
-        try {
-          await ddbClient.createTable(createParams).promise()
-        } catch (err) {
-          if (err && err.code !== "ResourceInUseException") {
-            throw err
-          }
-        }
-
-        await ddbClient.waitFor("tableExists", {
-          TableName: tableName,
-        }).promise();
-
+        return initDb();
     });
 
-
-
     test('handler', async () => {
-
 
         const startTime = new Date().getTime();
         const mockEvent: StoreMessageEventInput = {
@@ -78,8 +31,11 @@ describe("handler", () => {
             expect(result.deviceId).toEqual("F1234");
             expect(result.message).toEqual("Test Message");
             expect(result.messageType).toEqual("Test Message Type");
-            expect(result.timestamp).toBeGreaterThan(startTime);
+            expect(result.timestamp).toBeGreaterThanOrEqual(startTime);
         })
     });
 
+    afterAll(async () => {
+        teardownDb();
+    });
 });
---
+++ src/tests/testUtils/localDynamodb.ts
@@ -0,0 +1,78 @@
+import AWS from "aws-sdk";
+import bunyan from 'bunyan';
+
+const logger = bunyan.createLogger({name: "localDynamodb"});
+
+export const getDynamoDBClient = () => {
+    return new AWS.DynamoDB({
+        endpoint: "http://localhost:8000",
+        region: "ap-southeast-2"
+    });
+}
+
+
+export const initDb = (async () => {
+    logger.info("Initializing local dynamodb table for testing");
+
+    const ddbClient = getDynamoDBClient();
+    const tableName = process.env.DYNAMODB_TABLE || "";
+
+    try {
+      await ddbClient.deleteTable({TableName: tableName}).promise()
+    } catch (err) {
+      if (err && err.code !== "ResourceNotFoundException") {
+        throw err
+      }
+    }
+
+    await ddbClient.waitFor("tableNotExists", {
+      TableName: tableName,
+    }).promise()
+
+    const createParams: AWS.DynamoDB.Types.CreateTableInput = {
+      TableName: tableName,
+      AttributeDefinitions: [
+        {AttributeName: "deviceId", AttributeType: "S"},
+        {AttributeName: "timestamp", AttributeType: "N"},
+      ],
+      KeySchema: [
+        {AttributeName: "deviceId", KeyType: "HASH"},
+        {AttributeName: "timestamp", KeyType: "RANGE"},
+      ],
+      ProvisionedThroughput: {
+        ReadCapacityUnits: 2,
+        WriteCapacityUnits: 2,
+      },
+    }
+
+    try {
+      await ddbClient.createTable(createParams).promise()
+    } catch (err) {
+      if (err && err.code !== "ResourceInUseException") {
+        throw err
+      }
+    }
+
+    await ddbClient.waitFor("tableExists", {
+      TableName: tableName,
+    }).promise();
+
+});
+
+export const teardownDb = (async () => {
+    logger.info("Tearing down local dynamodb table");
+
+    const ddbClient = getDynamoDBClient();
+    const tableName = process.env.DYNAMODB_TABLE || "";
+
+    const deleteParams: AWS.DynamoDB.Types.DeleteTableInput = {
+        TableName: tableName
+    }
+
+    try {
+        await ddbClient.deleteTable(deleteParams).promise()
+    } catch (err) {
+        // TODO: handle error
+        logger.error("Error occurred while deleting local dynamodb table");
+    }
+});
