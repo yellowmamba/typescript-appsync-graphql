e4172aa1e57f1ff39d916b66b3a9cbe837aae161
Adam Quigley <adam.quigley@versent.com.au>
2019-05-22 15:54:58 +1000

    feat(resolver): add nested resolver for messages field on Device type

--- src/getDevice.ts
+++ src/getDevice.ts
@@ -19,19 +19,24 @@ export const handler = async (event: GetDeviceEventInput): Promise<any> => {
     logger.info(`Getting device from: ${TableName}`);
     logger.info(event);
 
-    const dbParams: DynamoDB.DocumentClient.GetItemInput = {
-        TableName,
-        Key: {
-            deviceId: event.arguments.deviceId
-        }
-    };
-
-    try {
-        const data = await dynamodb.get(dbParams).promise();
-        logger.info('Successfully got device:', data.Item);
-        return data.Item;
-    } catch (err) {
-        logger.error('ERROR:', err);
-        return err;
+    return {
+        "deviceId": event.arguments.deviceId,
+        "deviceName": "name"
     }
+
+    // const dbParams: DynamoDB.DocumentClient.GetItemInput = {
+    //     TableName,
+    //     Key: {
+    //         deviceId: event.arguments.deviceId
+    //     }
+    // };
+    //
+    // try {
+    //     const data = await dynamodb.get(dbParams).promise();
+    //     logger.info('Successfully got device:', data.Item);
+    //     return data.Item;
+    // } catch (err) {
+    //     logger.error('ERROR:', err);
+    //     return err;
+    // }
 }
